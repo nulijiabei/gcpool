41530301bb79ff5820be246bf33b1fbf66175f73
nljb <a@nljb.net>
2017-10-18 22:23:15 +0800

    sync.RWMutex -> sync.Map

--- core.go
+++ core.go
@@ -6,47 +6,64 @@ import (
 )
 
 type Core struct {
-	data map[string]interface{}
-	lock *sync.RWMutex
+	//	data map[string]interface{}
+	//	lock *sync.RWMutex
+	data sync.Map
 	name string
 }
 
 func NewCore(name string) *Core {
 	core := new(Core)
 	core.name = name
-	core.data = make(map[string]interface{})
-	core.lock = new(sync.RWMutex)
+	//	core.data = make(map[string]interface{})
+	//	core.lock = new(sync.RWMutex)
 	return core
 }
 
 func (this *Core) add(id string, obj interface{}) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
+	//	this.lock.Lock()
+	//	defer this.lock.Unlock()
+	//  log.Printf("Core(%s) add(%s)", this.name, id)
+	//	this.data[id] = obj
 	log.Printf("Core(%s) add(%s)", this.name, id)
-	this.data[id] = obj
+	this.data.Store(id, obj)
 }
 
 func (this *Core) len() int {
-	this.lock.RLock()
-	defer this.lock.RUnlock()
-	return len(this.data)
+	//	this.lock.RLock()
+	//	defer this.lock.RUnlock()
+	//	return len(this.data)
+	size := 0
+	this.data.Range(func(k, v interface{}) bool {
+		size++
+		return true
+	})
+	return size
 }
 
 func (this *Core) get(id string, callback func(obj interface{}) error) {
-	this.lock.RLock()
-	defer this.lock.RUnlock()
-	// log.Printf("Core(%s) get(%s)", this.name, id)
-	if obj, ok := this.data[id]; ok {
+	//	this.lock.RLock()
+	//	defer this.lock.RUnlock()
+	//	// log.Printf("Core(%s) get(%s)", this.name, id)
+	//	if obj, ok := this.data[id]; ok {
+	//		callback(obj)
+	//	}
+	if obj, ok := this.data.Load(id); ok {
 		callback(obj)
 	}
 }
 
 func (this *Core) del(id string, callback func(obj interface{})) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
+	//	this.lock.Lock()
+	//	defer this.lock.Unlock()
+	//	log.Printf("Core(%s) del(%s)", this.name, id)
+	//	if obj, ok := this.data[id]; ok {
+	//		callback(obj)
+	//		delete(this.data, id)
+	//	}
 	log.Printf("Core(%s) del(%s)", this.name, id)
-	if obj, ok := this.data[id]; ok {
+	if obj, ok := this.data.Load(id); ok {
 		callback(obj)
-		delete(this.data, id)
+		this.data.Delete(id)
 	}
 }
